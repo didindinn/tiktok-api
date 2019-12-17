ab16f8ada82b1a04593b8310df03595b00e4d2b9
Steffan Jensen <6hourapp@gmail.com>
2019-12-18 00:11:32 +0100

    Create example.py

---
+++ example.py
@@ -0,0 +1,12 @@
+import tiktokapi
+
+api = tiktokapi.api.Api()
+
+videos = api.get_user_videos("maskofshiva")
+
+for video in videos:
+    print(video)
+    print(api.get_meta_title(video))
+ #   print(api.get_video_homepage(video))
+    print(api.get_likes_count(video), "likes")
+    print(api.get_comment_count(video), "comments")
